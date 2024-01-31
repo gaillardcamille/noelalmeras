c7d0d327dadeae0b2af6d3cf3b75e97015c9739c
gaillardcamille <gaillard.camille48@gmail.com>
2024-01-31 20:32:30 +0100

    blabla

---
+++ src/pages/produit.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link href="../style/style.css" rel="stylesheet" />
+	<link href="../style/design_system.css" rel="stylesheet" />
+
+	<title>Noel Alméras</title>
+</head>
+
+<body>
+	<header>
+		<h1><a>Noël Alméras</a></h1>
+		<nav>
+			<ul>
+				<li><a href="index.php">Accueil</a></li>
+				<li><a href="#">Sève de Bouleau</a></li>
+				<li><a href="#">Autres produits</a></li>
+			</ul>
+		</nav>
+	</header>
+
+	<section id=image_bg
+	style='background-image: linear-gradient(0deg,
+	rgba(245, 255, 250, 1) 0%, rgba(255, 255, 255, 0) 100%),
+	url("../img/foret-bouleau.jpeg")'>
+	</section>
+
+	<section>
+
+	<section id="le_produit">
+
+		<img src="../img/noel.jpg" alt="Noël Alméras" />
+
+		<div>
+			<h2>Porte-clé champignon</h2>
+			<h3>L'unité</h3>
+			<p>3€</p>
+			<p>Disponible</p>
+			<button>Contacter</button>
+		</div>
+
+		</section>
+	</section>
+
+</body>
+
+</html>
