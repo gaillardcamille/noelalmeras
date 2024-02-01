342bcdeabe4169bc3090f3070ba8aee94711af5b
Hélie <bonjour@helie.me>
2024-02-02 00:29:35 +0100

    oui

--- src/component/footer.php
+++ src/component/footer.php
@@ -4,20 +4,14 @@
 
 	<section>
 
-		<div>
-			<ul>
-				<li>Accueil</li>
-				<li>Mentions Légales</li>
-				<li>Plan du site</li>
-			</ul>
-			<ul>
-				<li>+33 6 70 76 67 64</li>
-				<li>Mail</li>
-				<li>logo</li>
-			</ul>
-		</div>
+        <ul>
+            <li><a href="index">Accueil</a></li>
+            <li><a href="legals">Mentions Légales</a></li>
+            <li>+33 6 70 76 67 64</li>
+            <li>Mail : noelalmeras@pacpac.co.uk</li>
+        </ul>
 
-		<p>© 2024 - Deschamps Hélie / Gaillard Camille</p>
+        <p>© 2024 - <a href="https://www.linkedin.com/in/helie-deschamps/" target="_blank">Deschamps Hélie</a> / <a href="https://www.linkedin.com/in/gaillardcamille/" target="_blank">Gaillard Camille</a></p>
 
 	</section>
 
