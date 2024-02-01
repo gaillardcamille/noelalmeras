839d7964e2e34bc6eaf3057997829c8b447f1dd9
gaillardcamille <gaillard.camille48@gmail.com>
2024-02-01 21:47:06 +0100

    css

--- src/pages/sevedebouleau.php
+++ src/pages/sevedebouleau.php
@@ -42,6 +42,33 @@
 
 	</section>
 
+
+	<footer>
+
+		<img src="img/noel.jpg" alt="Photo de Noel Almeras">
+
+		<section>
+
+			<div>
+				<ul>
+					<li>Accueil</li>
+					<li>Mentions Légales</li>
+					<li>Plan du site</li>
+				</ul>
+				<ul>
+					<li>+33 6 70 76 67 64</li>
+					<li>Mail</li>
+					<li>logo</li>
+				</ul>
+			</div>
+
+			<p>© 2024 - Deschamps Hélie / Gaillard Camille</p>
+
+		</section>
+
+	</footer>
+
+
 </body>
 
 </html>
