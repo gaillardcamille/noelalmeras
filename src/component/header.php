08dbd569eaf5999318d632e40303bd169924954a
gaillardcamille <gaillard.camille48@gmail.com>
2024-02-01 23:24:10 +0100

    modif header

--- src/component/header.php
+++ src/component/header.php
@@ -8,8 +8,8 @@
 				<div href="#click" class="menu">
 					<a class="menu-title">Produits</a>
 					<ul class="menu-dropdown">
-						<a href="porte-cle-champignon.php">Porte clé champignon</a>
-						<a href="porte-cure-dents-champignon.php">Porte cure dents champignon</a>
+						<li><a href="porte-cle-champignon.php">Porte clé champignon</a></li>
+						<li><a href="porte-cure-dents-champignon.php">Porte cure dents champignon</a></li>
 					</ul>
 				</div>
 			</li>
